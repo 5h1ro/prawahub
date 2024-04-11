50a3ba531433175d81d87840543e85ad77098014
allburov <allburov@gmail.com>
2024-04-11 12:48:42 +0700

    dark theme by default

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
                     id: 'theme-css',
                     rel: 'stylesheet',
                     type: 'text/css',
-                    href: '/themes/aura-light-green/theme.css'
+                    href: '/themes/lara-dark-green/theme.css'
                 }
             ]
         }
