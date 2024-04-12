ba655b1e2fd031217aeeec79afab02a78d854a44
allburov <allburov@gmail.com>
2024-04-12 18:56:28 +0700

    Sessions

--- ui/components/ScreenshotButton.vue
+++ ui/components/ScreenshotButton.vue
@@ -1,9 +1,16 @@
 <script setup>
+const op = ref(null);
+const toggle = (event) => {
+  op.value.toggle(event);
+};
 
 </script>
 
 <template>
-  <Button icon="pi pi-camera" rounded outlined/>
+  <Button type="button" icon="pi pi-camera" @click="toggle" rounded outlined/>
+  <OverlayPanel ref="op" appendTo="body" :showCloseIcon="true">
+    <img src="/demo/images/nature/nature9.jpg" alt="Nature 9"/>
+  </OverlayPanel>
 </template>
 
 <style scoped lang="scss">
