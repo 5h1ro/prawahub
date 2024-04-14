7ffc03e92f0564c5e0549e65ec79763f0acfe69d
allburov <allburov@gmail.com>
2024-04-14 16:44:22 +0700

    refresh

--- ui/components/SessionControl.vue
+++ ui/components/SessionControl.vue
@@ -1,27 +1,45 @@
 <script setup>
+import {useToast} from "primevue/usetoast";
+
 defineProps(['session'])
+const toast = useToast();
+const refreshScreenshot = () => {
+  toast.add({severity: 'info', summary: 'Screenshot', detail: 'Refreshing screenshot'});
+}
 
 </script>
 
 <template>
   <Splitter style="min-height: 300px" class="mb-5">
     <SplitterPanel :size="40">
-      <div class="p-4">
-        <div class="flex justify-content-center">
-          <h6>Screenshot</h6>
+      <div class="px-4 pb-4">
+        <div class="flex justify-content-center align-items-center">
+          <h5 class="m-0">Screenshot</h5>
+          <RefreshButton
+              @click="refreshScreenshot"
+          ></RefreshButton>
         </div>
         <img src="/demo/images/nature/nature9.jpg" alt="Nature 9"
              style=" width:100%; height:100%"
         />
       </div>
     </SplitterPanel>
+
     <SplitterPanel :size="100">
       <Splitter layout="vertical">
         <SplitterPanel :size="50">
-          <div className="h-full flex align-items-center justify-content-center">Request</div>
+          <div class="p-2">
+            <div class="flex justify-content-center align-items-center">
+              <h5>Request</h5>
+            </div>
+          </div>
         </SplitterPanel>
         <SplitterPanel :size="50">
-          <div className="h-full flex align-items-center justify-content-center">Response</div>
+          <div class="p-2">
+            <div class="flex justify-content-center align-items-center">
+              <h5>Response</h5>
+            </div>
+          </div>
         </SplitterPanel>
       </Splitter>
     </SplitterPanel>
